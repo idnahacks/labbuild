e651bb06fefe9c0b6a760d36232e2d29f78bb545
thegoatreich <12153311+thegoatreich@users.noreply.github.com>
2021-11-16 13:56:48 +0000

    Update postinstalldc.ps1

--- postinstalldc.ps1
+++ postinstalldc.ps1
@@ -1,4 +1,4 @@
-new-item -path "c:\" -ItemType "directory" -Name "labscripts"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/thegoatreich/vulnerable-AD/master/vulnad.ps1" -outfile "c:\labscripts\vulnad.ps1"
+new-item -path "c:\" -ItemType "directory" -Name "labscripts" -force
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/thegoatreich/vulnerable-AD/master/vulnad.ps1" -outfile "c:\labscripts\vulnad.ps1" -force
 Import-Module "c:\labscripts\vulnad.ps1"
 Invoke-VulnAD -UsersLimit 666 -DomainName "lab.local"
