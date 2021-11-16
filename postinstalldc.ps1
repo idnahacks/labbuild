98cf5d27b69d517c1cc7eeb46b7d5a798598a7af
thegoatreich <12153311+thegoatreich@users.noreply.github.com>
2021-11-16 14:44:41 +0000

    Update postinstalldc.ps1

--- postinstalldc.ps1
+++ postinstalldc.ps1
@@ -4,6 +4,6 @@ if ((Test-Path -path "c:\labscripts\vulnad.ps1") -eq "$true") {
 if ((Test-Path -path "c:\labscripts") -eq "$false") {
     new-item -path "c:\" -ItemType "directory" -Name "labscripts" -force
 }
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wazehell/vulnerable-AD/master/vulnad.ps1" -outfile "c:\labscripts\vulnad.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/thegoatreich/vulnerable-AD/master/vulnad.ps1" -outfile "c:\labscripts\vulnad.ps1"
 Import-Module "c:\labscripts\vulnad.ps1"
 Invoke-VulnAD -UsersLimit 666 -DomainName "lab.local"
