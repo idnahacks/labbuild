38a526209db921068621e25a302802375d3ee849
thegoatreich <12153311+thegoatreich@users.noreply.github.com>
2021-11-16 14:01:50 +0000

    Update postinstalldc.ps1

--- postinstalldc.ps1
+++ postinstalldc.ps1
@@ -1,3 +1,4 @@
+remove-item -path "c:\labscripts\vulnad.ps1" -force
 new-item -path "c:\" -ItemType "directory" -Name "labscripts" -force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/thegoatreich/vulnerable-AD/master/vulnad.ps1" -outfile "c:\labscripts\vulnad.ps1" -force
 Import-Module "c:\labscripts\vulnad.ps1"
