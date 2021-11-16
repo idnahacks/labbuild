60056911e1f66a9ca59e3880532b69373f9d23fd
thegoatreich <12153311+thegoatreich@users.noreply.github.com>
2021-11-16 17:50:27 +0000

    Update postinstalldc.ps1

--- postinstalldc.ps1
+++ postinstalldc.ps1
@@ -1,9 +1,16 @@
+###Setup landing area###
+
 if ((Test-Path -path "c:\labscripts\vulnad.ps1") -eq $true) {
     remove-item -path "c:\labscripts\vulnad.ps1" -force
 }
 if ((Test-Path -path "c:\labscripts") -eq $false) {
     new-item -path "c:\" -ItemType "directory" -Name "labscripts" -force
 }
+
+###Vuln-AD
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/thegoatreich/vulnerable-AD/master/vulnad.ps1" -outfile "c:\labscripts\vulnad.ps1"
 Import-Module "c:\labscripts\vulnad.ps1"
 Invoke-VulnAD -UsersLimit 666 -DomainName "lab.local"
+
+###AD Tools
+add-windowsfeature -name RSAT-ADDS-Tools
